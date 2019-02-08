0d55b32b1fdcec54b2fe0633d8fc8153f600d4fa
Andrei Baicus <baicusandrei@me.com>
2019-02-09 00:49:55 +0200

    feat: add filter to allow the disabling of the onboarding #13

--- load.php
+++ load.php
@@ -6,6 +6,10 @@
  * @package themeisle-onboarding
  */
 
+if ( apply_filters( 'ti_onboarding_filter_module_status', true ) !== true ) {
+	return;
+}
+
 if ( ! class_exists( 'Themeisle_Onboarding' ) ) {
 	require_once dirname( __FILE__ ) . '/class-themeisle-onboarding.php';
 }
